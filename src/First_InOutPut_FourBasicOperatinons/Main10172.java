86b43c9aee45438447fd1ee72c32bcffe78e7d84
sion1212 <sion1212>
2024-06-24 12:16:32 +0900

    10172_개

--- src/First_InOutPut_FourBasicOperatinons/Main10172.java
+++ src/First_InOutPut_FourBasicOperatinons/Main10172.java
@@ -1,4 +1,24 @@
 package First_InOutPut_FourBasicOperatinons;
 
+import java.util.Scanner;
 public class Main10172 {
+    public static void main(String[] args){
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
 }
+/*
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
+ */
